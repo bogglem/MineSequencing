93ee3bcb3db24e5e067e1dba41d809d69fea7dbf
bogglem <36833875+bogglem@users.noreply.github.com>
2020-12-17 10:58:40 +1100

    tested

--- modeltest.py
+++ modeltest.py
@@ -11,17 +11,27 @@ from keras.models import Model, Sequential
 state_size=[15,15,7,3]
 action_size=15*15
 
-model2=Sequential()
+model2=Sequential(name='cnn')
 
-model2.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=state_size, padding='valid'))
+model2.add(Conv3D(16, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=state_size, padding='valid'))
 #model2.add(MaxPool3D((2,2,2), (1,1,1), padding='valid'))
-model2.add(MaxPool3D((2,2,2), (1,1,1), padding='valid'))
-model2.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=state_size, padding='valid'))
+#model2.add(MaxPool3D((2,2,2), (1,1,1), padding='valid'))
+model2.add(Conv3D(16, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=state_size, padding='valid'))
 model2.add(MaxPool3D((2,2,2), (1,1,1), padding='valid'))
 
 model2.add(Flatten())
+model2.add(Dense(16, activation='relu'))
 model2.add(Dense(16, activation='relu'))
 model2.add(Dense(32, activation='relu'))
 model2.add(Dense(action_size, activation='relu'))
 
 model2.summary()
+
+
+model3=Sequential(name='mlp')
+
+model3.add(Dense(64, activation='relu', input_shape=state_size))
+model3.add(Dense(64, activation='relu'))
+model3.add(Dense(action_size, activation='relu'))
+
+model3.summary()
