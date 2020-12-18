669b6551d0d347a916c6de5421f566b432edc110
bogglem <36833875+bogglem@users.noreply.github.com>
2020-12-18 15:02:53 +1100

    updated env reset

--- OPRG3Denv_gym.py
+++ OPRG3Denv_gym.py
@@ -241,13 +241,13 @@ class environment(gym.Env):
     
     def reset(self):
         
-        #if np.random.uniform()>0.005: #1/200 chance to create new environment
-        #    self.block_dic=deepcopy(self.block_dic_init)
-        #    self.ob_sample=deepcopy(self.norm)
+        if np.random.uniform()>0.005: #1/200 chance to create new environment
+            self.block_dic=deepcopy(self.block_dic_init)
+            self.ob_sample=deepcopy(self.norm)
             #self.render_update=deepcopy(self.geo_array[:,:,:,0])
         
-        #else:
-        self.build()
+        else:
+            self.build()
             
         
         self.turnore=0
