e51df02672871550d3ee988e53570ca68a190a50
bogglem <36833875+bogglem@users.noreply.github.com>
2021-12-03 21:30:09 +0800

    Update Train_curric_ACER_katana.py

--- Train_curric_ACER_katana.py
+++ Train_curric_ACER_katana.py
@@ -38,8 +38,8 @@ from tools.BMenv_curricturnspc import environment
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
-#idx=int(sys.argv[1]) #array row number. required for batch runs on pbs katana
-idx=5
+idx=int(sys.argv[1]) #array row number. required for batch runs on pbs katana
+#idx=5
 
 #prepare input parameters
 inputarray=pd.read_csv('jobarrays/curricturnspc_katana_job_input.csv')
