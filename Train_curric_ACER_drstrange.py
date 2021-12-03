9af52c73b104236b86a01692fa7eb82bfdf8e224
bogglem <36833875+bogglem@users.noreply.github.com>
2021-12-03 16:15:36 +0800

    Update Train_curric_ACER_drstrange.py

--- Train_curric_ACER_drstrange.py
+++ Train_curric_ACER_drstrange.py
@@ -36,7 +36,7 @@ from stable_baselines.common.callbacks import BaseCallback, CallbackList, EvalCa
 from stable_baselines import ACER
 from tools.SingleBMenv_dqncurricturnspc import environment
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '5'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 #idx=int(sys.argv[1]) #array row number. required for batch runs on pbs katana
 idx=5
