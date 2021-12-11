c4aa684036285737ba7976e4e33672d244e8a79f
bogglem <36833875+bogglem@users.noreply.github.com>
2021-12-11 14:34:44 +0800

    update

--- tools/loadsaveBMenv.py
+++ tools/loadsaveBMenv.py
@@ -20,7 +20,7 @@ from tools.createmodel import automodel
 
 class environment(gym.Env):
     
-    def __init__(self, x,y,z ,gamma, turnspc, policy, rg_prob=0.005, rendermode='off'):
+    def __init__(self, x,y,z ,gamma, turnspc, policy, rg_prob=0.005, annealrate=1000000, rendermode='off'):
         
         self.rendermode=rendermode # on/off display block model in matplotlib
        # self.cutoffpenaltyscalar=penaltyscalar #scaling parameter for changing the penalty for taking no action (cutoff).
@@ -33,7 +33,7 @@ class environment(gym.Env):
         self.saveddepdic='%s/depdict' % self.envpath
         self.savedeffdic='%s/effdict' % self.envpath
         self.policy=policy
-        
+        self.annealrate=annealrate
         #initiating values
         self.framecounter=0
         self.actionslist = list()
@@ -517,7 +517,7 @@ class environment(gym.Env):
         self.turncounter=0
         self.episodecounter+=1
         #increase number of turns available as training progresses
-        self.turns=min(round((len(self.dep_dic)*(self.dturnspc*self.episodecounter/1000000+self.startingturnspc))),round((len(self.dep_dic)*(self.dturnspc))))
+        self.turns=min(round((len(self.dep_dic)*(self.dturnspc*self.episodecounter/self.annealrate+self.startingturnspc))),round((len(self.dep_dic)*(self.dturnspc))))
 
         self.terminal=False
         self.i=-1
