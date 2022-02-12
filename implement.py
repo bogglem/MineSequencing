52740cf7335fc2d246f1eed0ce7a5aa743ad405b
bogglem <36833875+bogglem@users.noreply.github.com>
2022-02-12 19:07:49 +0800

    update

--- implement.py
+++ implement.py
@@ -50,12 +50,12 @@ policyname='MlpPolicy' #change this name to change RL policy type (MlpPolicy/Cnn
 if policyname == 'CnnPolicy':
     
     policy=CnnPolicy
-    test='CNNA2C'
+    test='CNNACER'
 
 elif policyname =='MlpPolicy':
 
     policy=MlpPolicy
-    test='MLPA2C'
+    test='MLPACER'
 
 trialv='exerror'
 #'loadsave10'
